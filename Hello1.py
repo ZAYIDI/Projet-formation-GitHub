6c9d715cbc60d059e5703680e2048934da42b1dc
Mhadjou ZAYIDI <zayidimhadjou10@gmail.com>
2024-04-16 16:35:31 +0400

    Hellooo

--- Hello1.py
+++ Hello1.py
@@ -11,3 +11,21 @@ print("Hello word 1!")
 
 
 print("Hello word 2!")
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.linspace(-2, 2, 100)
+
+y = np.sinc(2*x)
+
+
+plt.plot(x, y, 'b:*')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Sinus cardinal")
+plt.grid()
+plt.show()
